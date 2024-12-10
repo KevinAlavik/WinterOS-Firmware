7f1b9ee6e0f272537be46f08a3ae3aeba5c9f8f4
Kevin Alavik (Code Server) <kevin@alavik.se>
2024-12-10 13:43:22 +0100

    feat: Updated protocol

--- src/protocol.asm
+++ src/protocol.asm
@@ -3,5 +3,14 @@
 ; Forked version of the official firmware for WinterOS
 ; *****************************************************
 
-protocol_table:
+
+; ============================================
+; The WinterOS Firmware Protocol Table (WFPT).
+; ============================================
+WFPT:
+    ; To calculate the address of a function in the WFPT simply do:
+    ;  - address =  [WFTP_Base + (0x08 * idx)]
+
+    ; -- BEGIN --
     dq Console_Print    ; ProtocolTable[0] -> Print()
+    ; -- END --
